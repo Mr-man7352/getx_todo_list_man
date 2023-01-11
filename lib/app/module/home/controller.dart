ae53c1129426b0db24077db84bd8dca2ba4c85c9
Manish Sahu <manissh7352@gmail.com>
2023-01-12 03:41:27 +0530

    completed add todo to a task type

--- lib/app/module/home/controller.dart
+++ lib/app/module/home/controller.dart
@@ -12,6 +12,7 @@ class HomeController extends GetxController {
   final deleting = false.obs;
   final tasks = <Task>[].obs;
   final chipIndex = 0.obs;
+  final task = Rx<Task?>(null);
 
   @override
   void onInit() {
@@ -36,6 +37,28 @@ class HomeController extends GetxController {
 
   void deleteTask(Task task) {
     tasks.remove(task);
+  }
+
+  void changeTask(Task? select) {
+    task.value = select;
+  }
+
+  updateTask(Task task, String title) {
+    var todos = task.todos ?? [];
+    if (containTodo(todos, title)) {
+      return false;
+    }
+    var todo = {'title': title, 'done': false};
+    todos.add(todo);
+    var newTask = task.copyWith(todos: todos);
+    int oldIdx = tasks.indexOf(task);
+    tasks[oldIdx] = newTask;
+    tasks.refresh();
+    return true;
+  }
+
+  bool containTodo(List todos, String title) {
+    return todos.any((element) => element['title'] == title);
   }
 
   bool addTask(Task task) {
